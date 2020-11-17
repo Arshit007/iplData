5f1bb370aa8030bfc29f0da3bffcc8e8c6e028f5
Arshit Jain <arshit.jain@cloudsek.com>
2020-11-18 02:54:08 +0530

    Video Excel Added

--- ipl.py
+++ ipl.py
@@ -29,11 +29,11 @@ class IPL:
             iplAllStats.AllSeasonStats(data).prepare_data(fileName,data,f"/stats/all-time/",directory)
         delete_sheet('sheet1',os.path.join(directory,fileName))
         
-
+#690
     def get_season_range(self):
         return [ {"season":2008,"start":275,"end":334},{"season":2009,"start":216,"end":275},{"season":2010,"start":156,"end":216},
             {"season":2011,"start":82,"end":156},{"season":2012,"start":2,"end":78}, {"season":2013,"start":606,"end":607},
-            {"season":2013,"start":616,"end":651},{"season":2013,"start":652,"end":690},{"season":2013,"start":691,"end":693}, 
+            {"season":2013,"start":616,"end":651},{"season":2013,"start":652,"end":654},{"season":2013,"start":691,"end":693}, 
             {"season":2014,"start":2424,"end":2484},{"season":2015,"start":3226,"end":3286},{"season":2016,"start":4042,"end":4102},
             {"season":2017,"start":5839,"end":5899},{"season":2018,"start":7894,"end":7954},{"season":2019,"start":11137,"end":11154},
             {"season":2019,"start":11309,"end":11348},
@@ -41,9 +41,14 @@ class IPL:
 
     def generate_scorecards(self):
         for data in self.get_season_range():
-            iplScoreCards.ScoreCards(data['start'],data['end']).generate_file(f"dataset/allMatchDetail/{data['season']}/")
+            iplScoreCards.ScoreCards(data['start'],data['end']).create_match_details_directory(f"dataset/allMatchDetail/{data['season']}/")
+    
+    def generate_video_link(self):
+        for data in self.get_season_range()[7:]:
+            print(data)
+            iplScoreCards.ScoreCards(data['start'],data['end']).create_video_excel(f"dataset/allMatchVideoDetail/{data['season']}/",f"{data['season']}_{data['start']}")
+    
 
-
-IPL().get_all_season_stats()
-IPL().get_all_time_stats()
-IPL().generate_scorecards()
+# IPL().get_all_season_stats()
+# IPL().get_all_time_stats()
+IPL().generate_video_link()
--- iplScoreCards.py
+++ iplScoreCards.py
@@ -3,6 +3,10 @@ from utils import write_json_data
 from utils import get_detail_json
 import pathlib
 import os 
+import pandas as pd
+import json
+from io import StringIO
+
 class ScoreCards:
 
     def __init__(self,start_range,end_range):
@@ -13,17 +17,48 @@ class ScoreCards:
     def get_url(self,match_no):
         return f"https://cricketapi.platform.iplt20.com/fixtures/{match_no}/scoring"
     
-    
+    def get_video_link(self,url):
+        match_info = get_detail_json(url)['matchInfo']
+        additional_detail =  match_info['additionalInfo']
+        jobj={}
+        if('matchStatus' in match_info):
+            matchStatus = match_info['matchStatus']
+            if('text' in matchStatus):
+                jobj['result']= matchStatus['text']
+        if('toss.elected' in additional_detail):
+            jobj['toss']= additional_detail['toss.elected']
+        if('highlights.link' in additional_detail):
+            jobj['match_link']= str(additional_detail['highlights.link'])
+            return jobj
+            
 
     def get_file_name(self,url):
         return get_detail_json(url)['matchInfo']['tournamentLabel'].replace(" ","_")+"_"+get_detail_json(url)['matchInfo']['description'].replace(" ","_")
 
-    def generate_file(self,directory):
+    # def generate_file(self,directory):
+ 
+    #     self.create_video_excel(directory,)
+    
+    def create_match_details_directory(self,directory):
         for index in range(self.start,self.end):
             url = self.get_url(index)
             if not os.path.exists(directory):
                 os.makedirs(os.path.dirname(directory), exist_ok=True)
             write_json_data(os.path.join(directory,f"{self.get_file_name(url)}.json"),get_detail_json(url))
+
+    def create_video_excel(self,directory,season):
+        data=[]
+        for index in range(self.start,self.end):
+            url = self.get_url(index)
+            video_data = (self.get_video_link(url))
+            if (video_data):
+                data.append(video_data)
+        if data:
+            if not os.path.exists(directory):
+                os.makedirs(os.path.dirname(directory), exist_ok=True)
+            pd.read_json(StringIO(json.dumps(data))).to_excel(os.path.join(directory,f"{season}_season_highlights.xlsx"))
+
+    
                 
             
         
